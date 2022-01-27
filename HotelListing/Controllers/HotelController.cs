d7400379a6d3e14951bd316488dce541ba507f12
Arnold <arnoldschwazzy@gmail.com>
2022-01-27 09:52:01 +0000

    Added Country and Hotel Get Methids. Also fixed UnitOfWork DI Reference

--- HotelListing/Controllers/HotelController.cs
+++ HotelListing/Controllers/HotelController.cs
@@ -28,6 +28,8 @@ namespace HotelListing.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetHotels()
         {
             try
